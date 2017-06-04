0eccbe36387f8bba254bc7b0afee91058cbf7682
Andres Rios <rianby64@gmail.com>
2017-06-04 12:20:23 +0300

    [DEMO] Improve the way to interact

--- demo/demo-inline.js
+++ demo/demo-inline.js
@@ -6,31 +6,46 @@ var tmplCol = document.querySelector('template#col');
 var table = document.querySelector('table');
 var tbody = table.querySelector('tbody');
 
-function setupCol(el) {
-  var span = el.querySelector('span');
+function setupCol(el, key, entry) {
   var form = el.querySelector('form');
+  var span = el.querySelector('span');
+  span.textContent = entry[key];
+
+  var inputId = form.querySelector('input[name="id"]');
+  inputId.value = entry.id;
+
   var input = form.querySelector('input');
+  input.value = entry[key];
+  input.name = key;
 
   function toggle() {
     span.hidden = !span.hidden;
     form.hidden = !form.hidden;
+    if (!form.hidden) {
+      input.focus();
+    }
   }
 
   span.addEventListener('click', toggle);
+  input.addEventListener('blur', toggle);
 
   form.addEventListener('submit', e => {
-    toggle();
+    input.blur();
     e.preventDefault();
+
+    var fd = new FormData(e.target).toJSON();
   });
 }
 
-[1, 2, 3, 4, 5].forEach(() => {
-  var cloneRow = document.importNode(tmplRow.content, true);
-  [1, 2].forEach(() => {
-    var cloneCol = document.importNode(tmplCol.content, true);
-    setupCol(cloneCol);
-    cloneRow.appendChild(cloneCol);
+fetch('dummy.json').then(response => response.json()).then(entries => {
+  entries.forEach(entry => {
+    var cloneRow = document.importNode(tmplRow.content, true);
+    Object.keys(entry).forEach(key => {
+      var cloneCol = document.importNode(tmplCol.content, true);
+      setupCol(cloneCol, key, entry);
+      cloneRow.appendChild(cloneCol);
+    });
+    tbody.appendChild(cloneRow);
   });
-
-  tbody.appendChild(cloneRow);
 });
+
