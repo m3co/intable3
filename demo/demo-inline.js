7d21709e579d8070cd781b10740897f552f2296d
Andres Rios <rianby64@gmail.com>
2017-06-04 12:36:24 +0300

    [DEMO] Add the description to columns

--- demo/demo-inline.js
+++ demo/demo-inline.js
@@ -6,7 +6,7 @@ var tmplCol = document.querySelector('template#col');
 var table = document.querySelector('table');
 var tbody = table.querySelector('tbody');
 
-function setupCol(el, key, entry) {
+function setupCol(el, key, entry, description) {
   var form = el.querySelector('form');
   var span = el.querySelector('span');
   span.textContent = entry[key];
@@ -17,6 +17,7 @@ function setupCol(el, key, entry) {
   var input = form.querySelector('input');
   input.value = entry[key];
   input.name = key;
+  input.type = description.type;
 
   function toggle() {
     span.hidden = !span.hidden;
@@ -37,15 +38,16 @@ function setupCol(el, key, entry) {
   });
 }
 
-fetch('dummy.json').then(response => response.json()).then(entries => {
-  entries.forEach(entry => {
-    var cloneRow = document.importNode(tmplRow.content, true);
-    Object.keys(entry).forEach(key => {
-      var cloneCol = document.importNode(tmplCol.content, true);
-      setupCol(cloneCol, key, entry);
-      cloneRow.appendChild(cloneCol);
+fetch('describe-dummy.json').then(response => response.json()).then(columns => {
+  fetch('dummy.json').then(response => response.json()).then(entries => {
+    entries.forEach(entry => {
+      var cloneRow = document.importNode(tmplRow.content, true);
+      Object.keys(entry).forEach(key => {
+        var cloneCol = document.importNode(tmplCol.content, true);
+        setupCol(cloneCol, key, entry, columns[key]);
+        cloneRow.appendChild(cloneCol);
+      });
+      tbody.appendChild(cloneRow);
     });
-    tbody.appendChild(cloneRow);
   });
 });
-
