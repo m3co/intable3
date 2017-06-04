28c24d7bffa82838705fcd9c1e786dea267e7563
Andres Rios <rianby64@gmail.com>
2017-06-04 12:58:50 +0300

    [DEMO] Manage the columns configurations through a json file

--- demo/demo-inline.js
+++ demo/demo-inline.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var tmplThead = document.querySelector('template#thead');
+var tmplTheadCol = document.querySelector('template#thead-col');
 var tmplRow = document.querySelector('template#row');
 var tmplCol = document.querySelector('template#col');
 
@@ -7,6 +9,13 @@ var table = document.querySelector('table');
 var tbody = table.querySelector('tbody');
 
 function setupCol(el, key, entry, description) {
+  var td = el.querySelector('td');
+  description.type = description.type || 'text';
+  if (description.type === 'hidden') {
+    td.hidden = true;
+    return;
+  }
+
   var form = el.querySelector('form');
   var span = el.querySelector('span');
   span.textContent = entry[key];
@@ -35,17 +44,30 @@ function setupCol(el, key, entry, description) {
     e.preventDefault();
 
     var fd = new FormData(e.target).toJSON();
+    console.log(fd);
   });
 }
 
 fetch('describe-dummy.json').then(response => response.json()).then(columns => {
+  var cloneThead = document.importNode(tmplThead.content, true);
+  Object.keys(columns).forEach(key => {
+    var cloneTheadCol = document.importNode(tmplTheadCol.content, true);
+    var span = cloneTheadCol.querySelector('span');
+    span.textContent = columns[key].text;
+    if (columns[key].type === 'hidden') {
+      cloneTheadCol.querySelector('td').hidden = true;
+    }
+    cloneThead.querySelector('tr').appendChild(cloneTheadCol);
+  });
+  table.appendChild(cloneThead);
+
   fetch('dummy.json').then(response => response.json()).then(entries => {
     entries.forEach(entry => {
       var cloneRow = document.importNode(tmplRow.content, true);
       Object.keys(entry).forEach(key => {
         var cloneCol = document.importNode(tmplCol.content, true);
         setupCol(cloneCol, key, entry, columns[key]);
-        cloneRow.appendChild(cloneCol);
+        cloneRow.querySelector('tr').appendChild(cloneCol);
       });
       tbody.appendChild(cloneRow);
     });
