9ce7b91f7220476008d0d939009b45da8470d0d0
Andres Rios <rianby64@gmail.com>
2017-06-06 15:09:04 +0300

    [DEMO] Complete the way to delete a parent entry

--- demo/template-row.js
+++ demo/template-row.js
@@ -5,9 +5,14 @@
   var clone = document.importNode(tmpl.content, true);
 
   var table = clone.querySelector('x-inline-table');
-  var apuid = td.getAttribute('entry-apu-id');
-  table.setAttribute('url-index', '/api/datas/sliced?apuid=' + apuid);
+  var entryId = td.getAttribute('entry-id');
+  table.setAttribute('url-index', '/api/datas/sliced?apuid=' + entryId);
 
+  var tr1 = td.closest('tr');
+  var tr2 = tr1.previousElementSibling;
+  tr2.querySelector('#delete').addEventListener('click', () => {
+    tr1.remove();
+  });
   td.appendChild(clone);
 })();
 
